089260794b4e35cc795e084f3e0502da92c1079d
Lisa Zhang <lisa.zhang231@gmail.com>
2016-10-04 14:22:55 +0800

    fixed state selector

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,25 @@
+$( document ).ready(function() {
+
 //To link states to location selector
+var byStates = function() {
+  for (var key in states) {
+    $('<option>').val(key).text(states[key]).appendTo('#state');
+  }
+};
+byStates();
+
+
+// Search and fill by selected state
+var resultByState = function() {
+  var selectedState = $(var key in states).
+
+
+}
+
+
+
+
+
+
+
+});
